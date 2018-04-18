f4401de5e90011580c3fd80944dfb9c716c8fa71
nicolaz999 <nicolaz999@NICOLASRAGO>
2018-04-18 13:36:13 -0300

    Merge tpinvop/master

--- src/tpinvop/AdminBD.java
+++ src/tpinvop/AdminBD.java
@@ -1,28 +1,24 @@
-package InvOp;
+package tpinvop;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class AdminBD {
-    public void connectDatabase() {
+    private final String url = "jdbc:postgresql://localhost/dvdrental";
+    private final String user = "invop";
+    private final String password = "1234";
+    
+    public Connection connectDatabase() {
+        Connection conn = null;
         try {
-            // We register the PostgreSQL driver
-            // Registramos el driver de PostgresSQL
-            try { 
-                Class.forName("org.postgresql.Driver");
-            } catch (ClassNotFoundException ex) {
-                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
-            }
-            Connection connection = null;
-            // Database connect
-            // Conectamos con la base de datos
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/invopdb",
-                    "invop", "12345");
- 
-            boolean valid = connection.isValid(50000);
-            System.out.println(valid ? "TEST OK" : "TEST FAIL");
-        } catch (java.sql.SQLException sqle) {
-            System.out.println("Error: " + sqle);
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+ 
+        return conn;
     } 
 }
